4d38ccc09079e0e6d0cb76a8cbb7d78f365c47a1
Eol <eol@eolstudy.com>
2021-07-16 00:31:09 +0800

    update env

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,10 +20,6 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    indent: [
-      'error',
-      2
-    ],
     'linebreak-style': [
       'error',
       'unix'
@@ -38,6 +34,53 @@ module.exports = {
       {
         beforeStatementContinuationChars: 'any'
       }
-    ]
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',                                                   
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': [
+      'warn',
+      { 
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_'
+      } 
+    ],  
+    'quote-props': ['warn', 'as-needed'],
+    indent: ['warn', 2],
+    'no-console': [
+      'warn'
+    ],  
+    'no-debugger': [
+      'warn'
+    ],  
+    'no-useless-constructor': ['warn'],
+    'space-before-function-paren': ['warn', 'always'],
+    'space-before-blocks': ['warn', 'always'],
+    'space-infix-ops': 'warn',
+    'key-spacing': [
+      'warn',
+      {
+        beforeColon: false,
+        afterColon: true,
+        mode: 'strict'
+      }
+    ], 
+    'comma-spacing': [
+      'warn',
+      {
+        before: false,
+        after: true
+      }
+    ], 
+    'arrow-spacing': [
+      'warn',
+      {
+        before: true,
+        after: true
+      }
+    ], 
+    'object-curly-spacing': ['warn', 'always'],
+    'eol-last': ['warn', 'always']
   }
 }
